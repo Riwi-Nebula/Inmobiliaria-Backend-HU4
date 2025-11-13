6cbf91a3166d3baea7fd8faf8f0727df6c1b2abc
Farag0n <miguelanga1604@gmail.com>
2025-11-13 15:11:44 -0500

    feat: new atributes

--- Inmobiliaria-Backend-HU4.Domain/Entities/Property.cs
+++ Inmobiliaria-Backend-HU4.Domain/Entities/Property.cs
@@ -10,7 +10,9 @@ public class Property
     [Column(TypeName = "Varchar(100)")] public string Tittle { get; set; }
     [Column(TypeName = "varchar(900)")] public string Description { get; set; }
     public PropertyType? Type { get; set; }
-    [Column(TypeName = "varchar(500)")] public string Location { get; set; }
+    public PropertyState? State { get; set; }
+    [Column(TypeName = "varchar(100)")] public string Location { get; set; }
+    [Column(TypeName = "varchar(800)")] public string Address { get; set; }
     [Column(TypeName = "text")] public string PictureUrl { get; set; }
     [Column(TypeName = "decimal(5,2)")] public float Price { get; set; }
     public int OwnerId { get; set; }
